8433fdffc144d9bf9bd67665b094079af757038b
pinomaker-hoo <inhoo23@naver.com>
2023-08-30 21:43:05 +0900

    fix : Gulp Config

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const babel = require("gulp-babel");
 const swc = require("gulp-swc");
 const concat = require("gulp-concat");
+const ts = require("gulp-typescript");
+
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("compile-scss", function () {
   return gulp
     .src("src/style/**/*.scss")
     .pipe(sass())
-    .pipe(concat("styles.css"))
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build/style"));
+});
+
+gulp.task("compile-ts", function () {
+  return tsProject.src().pipe(tsProject()).pipe(gulp.dest("build"));
 });
 
 gulp.task("compile-js", function () {
@@ -27,11 +33,12 @@ gulp.task("copy-html", function () {
 
 gulp.task("watch", function () {
   gulp.watch("src/style/**/*.scss", gulp.series("compile-scss"));
+  gulp.watch("src/**/*.ts", gulp.series("compile-ts"));
   gulp.watch("src/**/*.js", gulp.series("compile-js"));
   gulp.watch("src/**/*.html", gulp.series("copy-html"));
 });
 
 gulp.task(
   "default",
-  gulp.series("compile-scss", "compile-js", "copy-html", "watch")
+  gulp.series("compile-scss", "compile-ts", "compile-js", "copy-html", "watch")
 );
