2147aab17cc7fd41e9c335e3a2b5caafd42d1d88
pinomaker-hoo <inhoo23@naver.com>
2023-09-01 12:44:29 +0900

    run : format

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,44 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const babel = require("gulp-babel");
-const swc = require("gulp-swc");
-const concat = require("gulp-concat");
-const ts = require("gulp-typescript");
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const babel = require('gulp-babel')
+const swc = require('gulp-swc')
+const concat = require('gulp-concat')
+const ts = require('gulp-typescript')
 
-const tsProject = ts.createProject("tsconfig.json");
+const tsProject = ts.createProject('tsconfig.json')
 
-gulp.task("compile-scss", function () {
+gulp.task('compile-scss', function () {
   return gulp
-    .src("src/style/**/*.scss")
+    .src('src/style/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest("build/style"));
-});
+    .pipe(gulp.dest('build/style'))
+})
 
-gulp.task("compile-ts", function () {
-  return tsProject.src().pipe(tsProject()).pipe(gulp.dest("build"));
-});
+gulp.task('compile-ts', function () {
+  return tsProject.src().pipe(tsProject()).pipe(gulp.dest('build'))
+})
 
-gulp.task("compile-js", function () {
+gulp.task('compile-js', function () {
   return gulp
-    .src("src/**/*.js")
+    .src('src/**/*.js')
     .pipe(babel())
     .pipe(swc())
-    .pipe(concat("bundle.js"))
-    .pipe(gulp.dest("build"));
-});
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('build'))
+})
 
-gulp.task("copy-html", function () {
-  return gulp.src("src/**/*.html").pipe(gulp.dest("build"));
-});
+gulp.task('copy-html', function () {
+  return gulp.src('src/**/*.html').pipe(gulp.dest('build'))
+})
 
-gulp.task("watch", function () {
-  gulp.watch("src/style/**/*.scss", gulp.series("compile-scss"));
-  gulp.watch("src/**/*.ts", gulp.series("compile-ts"));
-  gulp.watch("src/**/*.js", gulp.series("compile-js"));
-  gulp.watch("src/**/*.html", gulp.series("copy-html"));
-});
+gulp.task('watch', function () {
+  gulp.watch('src/style/**/*.scss', gulp.series('compile-scss'))
+  gulp.watch('src/**/*.ts', gulp.series('compile-ts'))
+  gulp.watch('src/**/*.js', gulp.series('compile-js'))
+  gulp.watch('src/**/*.html', gulp.series('copy-html'))
+})
 
 gulp.task(
-  "default",
-  gulp.series("compile-scss", "compile-ts", "compile-js", "copy-html", "watch")
-);
+  'default',
+  gulp.series('compile-scss', 'compile-ts', 'compile-js', 'copy-html', 'watch'),
+)
