dff6b280dd8453269933f5536201a3c59e3f604f
pinomaker-hoo <inhoo23@naver.com>
2023-09-26 14:59:38 +0900

    feat : Add Sampel Main Page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   function animateRandomly() {
-    gsap.to('.box img', {
+    gsap.to('#man', {
       x: () => Math.random() * (window.innerWidth - 100),
       y: () => Math.random() * (window.innerHeight - 100),
-      duration: 5,
+      duration: 7,
       onComplete: animateRandomly,
-      ease: 'none', // 속도 일정하게 유지
+      ease: 'none',
     })
   }
 
