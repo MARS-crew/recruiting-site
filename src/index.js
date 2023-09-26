18d1890c337e5f3dc48f642f96dc755bb76fdc91
pinomaker-hoo <inhoo23@naver.com>
2023-09-26 14:46:34 +0900

    feat : Add Sample Code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+document.addEventListener('DOMContentLoaded', function () {
+  function animateRandomly() {
+    gsap.to('.box img', {
+      x: () => Math.random() * (window.innerWidth - 100),
+      y: () => Math.random() * (window.innerHeight - 100),
+      duration: 5,
+      onComplete: animateRandomly,
+      ease: 'none', // 속도 일정하게 유지
+    })
+  }
+
+  animateRandomly()
+})
