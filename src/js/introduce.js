77a633359970f279128ca2a9c5e28ad1d3ca9640
gyul-ming <skm5078@kakao.com>
2023-10-11 22:08:50 +0900

    전체적인 틀 구성완료(PC버전)

--- src/js/introduce.js
+++ src/js/introduce.js
@@ -1,22 +1,37 @@
 const movingTextLeft = document.querySelector('.moving-text-left');
 const movingTextCenter = document.querySelector('.moving-text-center');
 const movingTextRight = document.querySelector('.moving-text-right');
-const titleText = document.querySelector('.content');
+const titleText = document.querySelector('.title');
+const introduceTextLeft = document.querySelector('.container');
+const introduceTextCenter = document.querySelector('.body-text-center');
+const introduceTextRight = document.querySelector('.body-text-right');
 
-movingTextLeft.addEventListener('click', () => {
-  gsap.to(window, {
-    duration: 1,
-    scrollTo: { y: titleText.offsetTop - 100 }, 
-    ease: 'power2.out'
-  });
-});
+introduceTextLeft.style.display = 'none';
+introduceTextCenter.style.display = 'none';
+introduceTextRight.style.display = 'none';
 
 // 우리는
 gsap.fromTo(
-    movingTextLeft, // 애니메이션을 적용할 요소 선택자
-    { x: -100, opacity: 1 }, // 시작 위치 및 초기 상태
-    { x: 450, opacity: 1, duration: 3, ease: 'power2.out' } // 종료 위치 및 애니메이션 속성
+    movingTextLeft, 
+    { x: -100, opacity: 1 }, 
+    { x: 450, opacity: 1, duration: 3, ease: 'power2.out' }
   );
+
+  movingTextLeft.addEventListener('click', () => {
+    titleText.style.display = 'none';
+    movingTextCenter.style.display = 'none';
+    movingTextRight.style.display = 'none';
+
+    introduceTextLeft.style.display = 'block';
+
+    gsap.to(movingTextLeft, {
+      x: 650,
+      y: -200,
+      opacity: 1,
+      duration: 2,
+      ease: 'power2.out'
+    });
+  });
 
 // 어디서
 gsap.fromTo(
@@ -25,10 +40,42 @@ gsap.fromTo(
     { x: 650, y: 0, opacity: 1, duration: 3, ease: 'power2.out' }
   );
 
+  movingTextCenter.addEventListener('click', () => {
+    titleText.style.display = 'none';
+    movingTextLeft.style.display = 'none';
+    movingTextRight.style.display = 'none';
+
+    introduceTextCenter.style.display = 'block';
+
+    gsap.to(movingTextCenter, {
+      x: 650,
+      y: -200,
+      opacity: 1,
+      duration: 2,
+      ease: 'power2.out'
+    });
+  });
+
   // 무엇을 할까
 gsap.fromTo(
     movingTextRight, 
     { x: 1200, y: 40, opacity: 1 },
     { x: 850, y: 0, opacity: 1, duration: 3, ease: 'power2.out'}
   );
+
+  movingTextRight.addEventListener('click', () => {
+    titleText.style.display = 'none';
+    movingTextLeft.style.display = 'none';
+    movingTextCenter.style.display = 'none';
+
+    introduceTextRight.style.display = 'block';
+
+    gsap.to(movingTextRight, {
+      x: 650,
+      y: -200,
+      opacity: 1,
+      duration: 2,
+      ease: 'power2.out'
+    });
+  });
   
