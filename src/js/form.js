4fdae008317a055bdf75c275062892d6806ef35b
kcl4419 <kcl4419@gmail.com>
2023-10-12 01:02:45 +0900

    feat : add mobile design

---
+++ src/js/form.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//   var currentPosition = parseInt($('.navigation').css('top'))
+//   $(window).scroll(function () {
+//     var position = $(window).scrollTop()
+//     $('.navigation')
+//       .stop()
+//       .animate({ top: position + currentPosition + 'px' }, 500)
+//   })
+// })
