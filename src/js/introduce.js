8bc121ad4a878fe1881e795ddd3ea25e51bb7a0b
gyul-ming <skm5078@kakao.com>
2023-10-14 00:02:34 +0900

    수정중

--- src/js/introduce.js
+++ src/js/introduce.js
@@ -2,31 +2,31 @@ const movingTextLeft = document.querySelector('.moving-text-left');
 const movingTextCenter = document.querySelector('.moving-text-center');
 const movingTextRight = document.querySelector('.moving-text-right');
 const titleText = document.querySelector('.title');
-const introduceTextLeft = document.querySelector('.container');
-const introduceTextCenter = document.querySelector('.body-text-center');
-const introduceTextRight = document.querySelector('.body-text-right');
+const marsImage = document.getElementById('mars');
+// const introduceTextLeft = document.querySelector('.container');
+// const introduceTextCenter = document.querySelector('.body-text-center');
+// const introduceTextRight = document.querySelector('.body-text-right');
 
-introduceTextLeft.style.display = 'none';
-introduceTextCenter.style.display = 'none';
-introduceTextRight.style.display = 'none';
+
 
 // 우리는
 gsap.fromTo(
     movingTextLeft, 
-    { x: -100, opacity: 1 }, 
-    { x: 450, opacity: 1, duration: 3, ease: 'power2.out' }
+    { x: '-500%', y: 0, opacity: 1 }, 
+    { x: '-20%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
   );
 
-  movingTextLeft.addEventListener('click', () => {
+    //우리는 클릭 이벤트
+    movingTextLeft.addEventListener('click', () => {
     titleText.style.display = 'none';
     movingTextCenter.style.display = 'none';
     movingTextRight.style.display = 'none';
 
-    introduceTextLeft.style.display = 'block';
+    //introduceTextLeft.style.display = 'block';
 
     gsap.to(movingTextLeft, {
-      x: 650,
-      y: -200,
+      x: '10%',
+      y: -250,
       opacity: 1,
       duration: 2,
       ease: 'power2.out'
@@ -36,20 +36,21 @@ gsap.fromTo(
 // 어디서
 gsap.fromTo(
     movingTextCenter, 
-    { x: 650, y: 100, opacity: 1 },
-    { x: 650, y: 0, opacity: 1, duration: 3, ease: 'power2.out' }
+    { x: '30%', y: 300, opacity: 1 },
+    { x: '50%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
   );
 
+  //어디서 클릭 이벤트
   movingTextCenter.addEventListener('click', () => {
     titleText.style.display = 'none';
     movingTextLeft.style.display = 'none';
     movingTextRight.style.display = 'none';
 
-    introduceTextCenter.style.display = 'block';
+    //introduceTextCenter.style.display = 'block';
 
     gsap.to(movingTextCenter, {
-      x: 650,
-      y: -200,
+      x: '10%',
+      y: -250,
       opacity: 1,
       duration: 2,
       ease: 'power2.out'
@@ -59,24 +60,33 @@ gsap.fromTo(
   // 무엇을 할까
 gsap.fromTo(
     movingTextRight, 
-    { x: 1200, y: 40, opacity: 1 },
-    { x: 850, y: 0, opacity: 1, duration: 3, ease: 'power2.out'}
+    { x: '400%', y: 40, opacity: 1 },
+    { x: '60%', y: 0, opacity: 1, duration: 5, ease: 'power2.out'}
   );
 
+  //무엇을 할까 클릭 이벤트
   movingTextRight.addEventListener('click', () => {
     titleText.style.display = 'none';
     movingTextLeft.style.display = 'none';
     movingTextCenter.style.display = 'none';
 
-    introduceTextRight.style.display = 'block';
+    //introduceTextRight.style.display = 'block';
 
     gsap.to(movingTextRight, {
-      x: 650,
-      y: -200,
+      x: '10%',
+      y: -250,
       opacity: 1,
       duration: 2,
       ease: 'power2.out'
     });
+  });
+
+
+  //화성 이동 애니메이션
+  gsap.to(marsImage, {
+    y: '200px', 
+    duration: 2, 
+    delay: 2  
   });
   
   //뒤로 가기
@@ -87,12 +97,12 @@ gsap.fromTo(
     movingTextRight.style.display = 'block';
     
     // 각 섹션의 정보를 초기 상태로 돌림
-    introduceTextLeft.style.display = 'none';
-    introduceTextCenter.style.display = 'none';
-    introduceTextRight.style.display = 'none';
+    // introduceTextLeft.style.display = 'none';
+    // introduceTextCenter.style.display = 'none';
+    // introduceTextRight.style.display = 'none';
   
     // 각 텍스트를 초기 위치로 이동
-    gsap.to(movingTextLeft, { x: 450, y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
-    gsap.to(movingTextCenter, { x: 650, y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
-    gsap.to(movingTextRight, { x: 850, y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
+    gsap.to(movingTextLeft, { x: '-30%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
+    gsap.to(movingTextCenter, { x: '50%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
+    gsap.to(movingTextRight, { x: '70%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
   }
