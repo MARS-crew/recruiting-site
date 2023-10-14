7c17f10655e1e727b598bd928fadbef4e03cf25a
unknown <young01229@naver.com>
2023-10-14 19:53:39 +0900

    feat : middle gallery 개발

--- src/js/script.js
+++ src/js/script.js
@@ -57,9 +57,6 @@ function scrubTo(totalTime) { // moves the scroll position to the place that cor
 	}
 }
 
-document.querySelector(".next").addEventListener("click", () => scrubTo(scrub.vars.totalTime + spacing));
-document.querySelector(".prev").addEventListener("click", () => scrubTo(scrub.vars.totalTime - spacing));
-
 
 
 
@@ -80,7 +77,7 @@ function buildSeamlessLoop(items, spacing) {
 		i, index, item;
 
 	// set initial state of items
-	gsap.set(items, {xPercent: 400, opacity: 0,	scale: 0});
+	gsap.set(items, {xPercent: 450, opacity: 0,	scale: 0});
 
 	// now loop through and create all the animations in a staggered fashion. Remember, we must create EXTRA animations at the end to accommodate the seamless looping.
 	for (i = 0; i < l; i++) {
@@ -88,7 +85,7 @@ function buildSeamlessLoop(items, spacing) {
 		item = items[index];
 		time = i * spacing;
 		rawSequence.fromTo(item, {scale: 0, opacity: 0}, {scale: 1, opacity: 1, zIndex: 100, duration: 0.5, yoyo: true, repeat: 1, ease: "power1.in", immediateRender: false}, time)
-		           .fromTo(item, {xPercent: 400}, {xPercent: -400, duration: 1, ease: "none", immediateRender: false}, time);
+		           .fromTo(item, {xPercent: 450}, {xPercent: -450, duration: 1, ease: "none", immediateRender: false}, time);
 		i <= items.length && seamlessLoop.add("label" + i, time); // we don't really need these, but if you wanted to jump to key spots using labels, here ya go.
 	}
 	
