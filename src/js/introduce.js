6dbc8b2329906f87ab55cf891fd03c6399d3c7cb
gyul-ming <skm5078@kakao.com>
2023-10-15 02:10:22 +0900

    PC버전 수정 완료 (배경화면 x)

--- src/js/introduce.js
+++ src/js/introduce.js
@@ -3,11 +3,6 @@ const movingTextCenter = document.querySelector('.moving-text-center');
 const movingTextRight = document.querySelector('.moving-text-right');
 const titleText = document.querySelector('.title');
 const marsImage = document.getElementById('mars');
-// const introduceTextLeft = document.querySelector('.container');
-// const introduceTextCenter = document.querySelector('.body-text-center');
-// const introduceTextRight = document.querySelector('.body-text-right');
-
-
 
 // 우리는
 gsap.fromTo(
@@ -16,69 +11,112 @@ gsap.fromTo(
     { x: '-20%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
   );
 
-    //우리는 클릭 이벤트
-    movingTextLeft.addEventListener('click', () => {
-    titleText.style.display = 'none';
+  // 우리는 클릭 이벤트
+  // movingTextLeft.addEventListener('click', () => {
+  // titleText.style.display = 'none';
+  // movingTextCenter.style.display = 'none';
+  // movingTextRight.style.display = 'none';
+
+  //   //introduceTextLeft.style.display = 'block';
+
+  //   gsap.to(movingTextLeft, {
+  //     x: '0%',
+  //     y: -250,
+  //     opacity: 1,
+  //     duration: 2,
+  //     fontSize: '65px',
+  //     color: 'white',
+  //     ease: 'power2.out'
+  //   });
+  // });
+
+  // 타이틀 텍스트 변환 함수
+  function changeTitleText(a) {
+    if (a == 0) {
+      titleText.innerText = '마스외전으로 놀러와!';
+    } else if (a == 1) {
+      titleText.innerText = '우리는'; 
+    } else if (a == 2) {
+      titleText.innerText = '어디서';
+    } else if (a == 3) {
+      titleText.innerText = '무엇을 할까';
+    }
+  }
+
+  function clickDisplay() {
+    movingTextLeft.style.display = 'none';    
     movingTextCenter.style.display = 'none';
     movingTextRight.style.display = 'none';
-
-    //introduceTextLeft.style.display = 'block';
-
-    gsap.to(movingTextLeft, {
-      x: '10%',
-      y: -250,
-      opacity: 1,
-      duration: 2,
-      ease: 'power2.out'
-    });
+  }
+  
+  // 우리는 클릭 이벤트
+  movingTextLeft.addEventListener('click', () => {
+    changeTitleText(1);
+    clickDisplay();
   });
 
 // 어디서
 gsap.fromTo(
-    movingTextCenter, 
-    { x: '30%', y: 300, opacity: 1 },
-    { x: '50%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
-  );
+  movingTextCenter, 
+  { x: '30%', y: 300, opacity: 1 },
+  { x: '30%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
+);
 
-  //어디서 클릭 이벤트
+  // 어디서 클릭 이벤트
+  // movingTextCenter.addEventListener('click', () => {
+  // titleText.style.display = 'none';
+  // movingTextLeft.style.display = 'none';
+  // movingTextRight.style.display = 'none';
+
+  // //introduceTextCenter.style.display = 'block';
+  // movingTextRight.style.fontSize = '65px';
+  // movingTextRight.style.color = 'white';
+
+  //   gsap.to(movingTextCenter, {
+  //     x: '0%',
+  //     y: -250,
+  //     opacity: 1,
+  //     duration: 2,
+  //     ease: 'power2.out'
+  //   });
+  // });
+
+  // 어디서 클릭 이벤트
   movingTextCenter.addEventListener('click', () => {
-    titleText.style.display = 'none';
-    movingTextLeft.style.display = 'none';
-    movingTextRight.style.display = 'none';
-
-    //introduceTextCenter.style.display = 'block';
-
-    gsap.to(movingTextCenter, {
-      x: '10%',
-      y: -250,
-      opacity: 1,
-      duration: 2,
-      ease: 'power2.out'
-    });
+    changeTitleText(2);
+    clickDisplay();
   });
 
   // 무엇을 할까
-gsap.fromTo(
+  gsap.fromTo(
     movingTextRight, 
     { x: '400%', y: 40, opacity: 1 },
-    { x: '60%', y: 0, opacity: 1, duration: 5, ease: 'power2.out'}
+    { x: '40%', y: 0, opacity: 1, duration: 5, ease: 'power2.out'}
   );
 
-  //무엇을 할까 클릭 이벤트
+  // 무엇을 할까 클릭 이벤트
+  // movingTextRight.addEventListener('click', () => {
+  // titleText.style.display = 'none';
+  // movingTextLeft.style.display = 'none';
+  // movingTextCenter.style.display = 'none';
+
+  // //introduceTextRight.style.display = 'block';
+  // movingTextRight.style.fontSize = '65px';
+  // movingTextRight.style.color = 'white';
+
+  //   gsap.to(movingTextRight, {
+  //     x: '0%',
+  //     y: -250,
+  //     opacity: 1,
+  //     duration: 2,
+  //     ease: 'power2.out'
+  //   });
+  // });
+
+  // 무엇을 할까 클릭 이벤트
   movingTextRight.addEventListener('click', () => {
-    titleText.style.display = 'none';
-    movingTextLeft.style.display = 'none';
-    movingTextCenter.style.display = 'none';
-
-    //introduceTextRight.style.display = 'block';
-
-    gsap.to(movingTextRight, {
-      x: '10%',
-      y: -250,
-      opacity: 1,
-      duration: 2,
-      ease: 'power2.out'
-    });
+    changeTitleText(3);
+    clickDisplay();
   });
 
 
@@ -91,18 +129,8 @@ gsap.fromTo(
   
   //뒤로 가기
   function goBack() {
-    titleText.style.display = 'block';
+    changeTitleText(0);
     movingTextLeft.style.display = 'block';
     movingTextCenter.style.display = 'block';
     movingTextRight.style.display = 'block';
-    
-    // 각 섹션의 정보를 초기 상태로 돌림
-    // introduceTextLeft.style.display = 'none';
-    // introduceTextCenter.style.display = 'none';
-    // introduceTextRight.style.display = 'none';
-  
-    // 각 텍스트를 초기 위치로 이동
-    gsap.to(movingTextLeft, { x: '-30%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
-    gsap.to(movingTextCenter, { x: '50%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
-    gsap.to(movingTextRight, { x: '70%', y: 0, opacity: 1, duration: 3, ease: 'power2.out'});
   }
