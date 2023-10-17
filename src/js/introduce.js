62ef8ea967772cab02ed9745d02ade0c6aaef028
pinomaker-hoo <inhoo23@naver.com>
2023-10-17 10:18:27 +0900

    feat : Run Format

--- src/js/introduce.js
+++ src/js/introduce.js
@@ -1,96 +1,96 @@
-const movingTextLeft = document.querySelector('.moving-text-left');
-const movingTextCenter = document.querySelector('.moving-text-center');
-const movingTextRight = document.querySelector('.moving-text-right');
-const movingTextTop = document.querySelector('.moving-text-top');
-const movingTextMiddle = document.querySelector('.moving-text-middle');
-const movingTextBottom = document.querySelector('.moving-text-bottom');
-const titleText = document.querySelector('.title');
-const marsImage = document.getElementById('mars');
-const isMobile = window.innerWidth <= 767;
+const movingTextLeft = document.querySelector('.moving-text-left')
+const movingTextCenter = document.querySelector('.moving-text-center')
+const movingTextRight = document.querySelector('.moving-text-right')
+const movingTextTop = document.querySelector('.moving-text-top')
+const movingTextMiddle = document.querySelector('.moving-text-middle')
+const movingTextBottom = document.querySelector('.moving-text-bottom')
+const titleText = document.querySelector('.title')
+const marsImage = document.getElementById('mars')
+const isMobile = window.innerWidth <= 767
 
-if(!isMobile){
+if (!isMobile) {
   // 타이틀 텍스트 변환 함수
   function changeTitleText(a) {
     if (a == 0) {
-      titleText.innerText = '마스외전으로 놀러와!';
+      titleText.innerText = '마스외전으로 놀러와!'
     } else if (a == 1) {
-      titleText.innerText = '우리는'; 
+      titleText.innerText = '우리는'
     } else if (a == 2) {
-      titleText.innerText = '어디서';
+      titleText.innerText = '어디서'
     } else if (a == 3) {
-      titleText.innerText = '무엇을 할까';
+      titleText.innerText = '무엇을 할까'
     }
   }
 
   function clickDisplay() {
-    movingTextLeft.style.display = 'none';    
-    movingTextCenter.style.display = 'none';
-    movingTextRight.style.display = 'none';
+    movingTextLeft.style.display = 'none'
+    movingTextCenter.style.display = 'none'
+    movingTextRight.style.display = 'none'
   }
 
   // 우리는
   gsap.fromTo(
-  movingTextLeft, 
-    { x: '-500%', y: 0, opacity: 1 }, 
-    { x: '-20%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
-  );
-  
+    movingTextLeft,
+    { x: '-500%', y: 0, opacity: 1 },
+    { x: '-20%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' },
+  )
+
   // 우리는 클릭 이벤트
   movingTextLeft.addEventListener('click', () => {
-    changeTitleText(1);
-    clickDisplay();
-  });
+    changeTitleText(1)
+    clickDisplay()
+  })
 
   // 어디서
   gsap.fromTo(
-    movingTextCenter, 
+    movingTextCenter,
     { x: '30%', y: 300, opacity: 1 },
-    { x: '30%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' }
-  );
+    { x: '30%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' },
+  )
 
   // 어디서 클릭 이벤트
   movingTextCenter.addEventListener('click', () => {
-    changeTitleText(2);
-    clickDisplay();
-  });
+    changeTitleText(2)
+    clickDisplay()
+  })
 
   // 무엇을 할까
   gsap.fromTo(
-    movingTextRight, 
+    movingTextRight,
     { x: '400%', y: 40, opacity: 1 },
-    { x: '40%', y: 0, opacity: 1, duration: 5, ease: 'power2.out'}
-  );
+    { x: '40%', y: 0, opacity: 1, duration: 5, ease: 'power2.out' },
+  )
 
   // 무엇을 할까 클릭 이벤트
   movingTextRight.addEventListener('click', () => {
-    changeTitleText(3);
-    clickDisplay();
-  });
+    changeTitleText(3)
+    clickDisplay()
+  })
 
   //화성 이동 애니메이션
   gsap.to(marsImage, {
-    y: '200px', 
-    duration: 2, 
-    delay: 2  
-  });
-  
+    y: '200px',
+    duration: 2,
+    delay: 2,
+  })
+
   //뒤로 가기
   function goBack() {
-    changeTitleText(0);
-    movingTextLeft.style.display = 'block';
-    movingTextCenter.style.display = 'block';
-    movingTextRight.style.display = 'block';
+    changeTitleText(0)
+    movingTextLeft.style.display = 'block'
+    movingTextCenter.style.display = 'block'
+    movingTextRight.style.display = 'block'
   }
 }
 
-if(isMobile){
+if (isMobile) {
   //1번째 애니메이션 '우리는'
   gsap.to(movingTextTop, {
     x: '130%', // x축으로 이동할 거리
     y: '-50%', // y축으로 이동할 거리
     duration: 2, // 애니메이션 기간 (초)
-    ease: 'power1.inOut' // 이징 함수
-  });
+    ease: 'power1.inOut', // 이징 함수
+  })
 
   //1번째 애니메이션 '어디서'
   gsap.to(movingTextMiddle, {
@@ -101,47 +101,47 @@ if(isMobile){
       values: [
         { x: '-20%', y: '0%' }, // 시작 지점
         { x: '100%', y: '-25%' }, // 중간 지점
-        { x: '130%', y: '-50%' } // 끝 지점
-      ]
+        { x: '130%', y: '-50%' }, // 끝 지점
+      ],
     },
     duration: 2, // 애니메이션 기간 (초)
-    ease: 'power1.inOut' // 이징 함수
-  });
+    ease: 'power1.inOut', // 이징 함수
+  })
 
   // big-circle를 2초 후에 사라지도록 애니메이션 설정
   gsap.to('.big-circle', {
-  opacity: 0,
-  delay: 2,
-  onComplete: () => {
-    document.querySelector('.big-circle').style.display = 'none';
-  }
-    });
+    opacity: 0,
+    delay: 2,
+    onComplete: () => {
+      document.querySelector('.big-circle').style.display = 'none'
+    },
+  })
 
   // small-circle를 2초 후에 사라지도록 애니메이션 설정
   gsap.to('.small-circle', {
-  opacity: 0,
-  delay: 2,
-  onComplete: () => {
-    document.querySelector('.small-circle').style.display = 'none';
-  }
-    });
+    opacity: 0,
+    delay: 2,
+    onComplete: () => {
+      document.querySelector('.small-circle').style.display = 'none'
+    },
+  })
 
-  //2번째 애니메이션 '우리가'  
+  //2번째 애니메이션 '우리가'
   gsap.to(movingTextTop, {
-    x: '125%', 
-    y: '-40%', 
+    x: '125%',
+    y: '-40%',
     duration: 2, // 애니메이션 기간 (초)
-    ease: 'power1.inOut',// 이징 함수
-    delay: 2.5
-  });
+    ease: 'power1.inOut', // 이징 함수
+    delay: 2.5,
+  })
 
   gsap.to(movingTextMiddle, {
-    x: '25%', 
+    x: '25%',
     y: '30%', // y축으로 이동할 거리
     duration: 2, // 애니메이션 기간 (초)
-    ease: 'power1.inOut',// 이징 함수
-    delay: 2.5
-  });
+    ease: 'power1.inOut', // 이징 함수
+    delay: 2.5,
+  })
 
   gsap.to(movingTextBottom, {
     x: '-60%', // x축으로 이동할 거리
@@ -151,43 +151,42 @@ if(isMobile){
       values: [
         { x: '-20%', y: '0%' }, // 시작 지점
         { x: '100%', y: '-25%' }, // 중간 지점
-        { x: '130%', y: '-50%' } // 끝 지점
-      ]
+        { x: '130%', y: '-50%' }, // 끝 지점
+      ],
     },
     duration: 2, // 애니메이션 기간 (초)
-    ease: 'power1.inOut',// 이징 함수
-    delay: 2.5
-  });
+    ease: 'power1.inOut', // 이징 함수
+    delay: 2.5,
+  })
 
   gsap.to(marsImage, {
     y: '-60%', // 위로 올라가는 거리
     scale: 2, // 크기를 1.5배로 키움
     duration: 2.5, // 애니메이션 기간
     delay: 3,
-    ease: 'power2.out' // 이징 함수
-  });
+    ease: 'power2.out', // 이징 함수
+  })
 
   function clickDisplay() {
-    titleText.style.display='none';
-    movingTextTop.style.display = 'none';    
-    movingTextMiddle.style.display = 'none';
-    movingTextBottom.style.display = 'none';
-    marsImage.style.display='none';
+    titleText.style.display = 'none'
+    movingTextTop.style.display = 'none'
+    movingTextMiddle.style.display = 'none'
+    movingTextBottom.style.display = 'none'
+    marsImage.style.display = 'none'
   }
 
-   // 우리는 클릭 이벤트
-   movingTextTop.addEventListener('click', () => {
-    clickDisplay();
-  });
+  // 우리는 클릭 이벤트
+  movingTextTop.addEventListener('click', () => {
+    clickDisplay()
+  })
 
   // 우리는 클릭 이벤트
   movingTextMiddle.addEventListener('click', () => {
-    clickDisplay();
-  });
+    clickDisplay()
+  })
 
   // 우리는 클릭 이벤트
   movingTextBottom.addEventListener('click', () => {
-    clickDisplay();
-  });
-} 
-
+    clickDisplay()
+  })
+}
