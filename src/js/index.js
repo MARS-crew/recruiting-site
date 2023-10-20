cbce847fb4b5dfc7abb9d517b49d466147c4e7d6
pinomaker-hoo <inhoo23@naver.com>
2023-10-21 04:35:38 +0900

    fix :

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function animateRandomly() {
+    gsap.to('#man', {
+      x: () => Math.random() * (window.innerWidth - 100),
+      y: () => Math.random() * (window.innerHeight - 100),
+      duration: 5,
+      onComplete: animateRandomly,
+      ease: 'none',
+    })
+  }
+
+  animateRandomly()
+})
+
+const sections = document.querySelectorAll('.section')
+let currentSection = 0
+let isScrolling = false
+
+function scrollToSection(index) {
+  if (index >= 0 && index < sections.length) {
+    sections[index].scrollIntoView({ behavior: 'smooth' })
+    currentSection = index
+  }
+}
+
+window.addEventListener('wheel', (e) => {
+  if (!isScrolling) {
+    if (e.deltaY > 0) {
+      scrollToSection(currentSection + 1)
+    } else if (e.deltaY < 0) {
+      scrollToSection(currentSection - 1)
+    }
+    isScrolling = true
+    setTimeout(() => {
+      isScrolling = false
+    }, 1000)
+  }
+})
