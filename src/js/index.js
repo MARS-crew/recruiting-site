1d44d06217e54ee358b0c9c79e0a7ac69251422b
pinomaker-hoo <inhoo23@naver.com>
2023-10-26 01:10:52 +0900

    feat : index 리펙토링

--- src/js/index.js
+++ src/js/index.js
@@ -2,28 +2,21 @@ const sections = document.querySelectorAll('.section')
 let currentSection = 0
 let isScrolling = false
 
-function scrollToSection(index) {
+/**
+ * 스크롤 함수
+ * @param {number} index
+ */
+const scrollToSection = (index) => {
   if (index >= 0 && index < sections.length) {
     sections[index].scrollIntoView({ behavior: 'smooth' })
     currentSection = index
   }
 }
 
-window.addEventListener('wheel', (e) => {
-  if (!isScrolling) {
-    if (e.deltaY > 0) {
-      scrollToSection(currentSection + 1)
-    } else if (e.deltaY < 0) {
-      scrollToSection(currentSection - 1)
-    }
-    isScrolling = true
-    setTimeout(() => {
-      isScrolling = false
-    }, 1000)
-  }
-})
-
-function animateRandomly() {
+/**
+ * 우주인 애니메이션 함수
+ */
+const animateRandomly = () => {
   gsap.to('#man', {
     x: () => Math.random() * (window.innerWidth - 100),
     y: () => Math.random() * (window.innerHeight - 100),
@@ -32,6 +25,23 @@ function animateRandomly() {
     ease: 'none',
   })
 }
+
+window.addEventListener('wheel', (e) => {
+  if (isScrolling) return
+
+  if (e.deltaY > 0) {
+    scrollToSection(currentSection + 1)
+  }
+
+  if (e.deltaY < 0) {
+    scrollToSection(currentSection - 1)
+  }
+
+  isScrolling = true
+  setTimeout(() => {
+    isScrolling = false
+  }, 1000)
+})
 
 document.addEventListener('DOMContentLoaded', function () {
   animateRandomly()
