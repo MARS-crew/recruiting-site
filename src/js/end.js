9126af5a6de9b7e9d671f360171a35cb0d08d266
pinomaker-hoo <inhoo23@naver.com>
2023-11-01 00:20:44 +0900

    feat : End Page

--- src/js/end.js
+++ src/js/end.js
@@ -2,52 +2,7 @@ const content = document.querySelector('.content')
 const targetElement = document.querySelector('.start_point')
 const scroll = document.querySelector('.scrolling-text')
 const end = document.querySelector('.stop_point')
-
-let scrollPosition = 0
-
-// window.addEventListener('DOMContentLoaded', function () {
-//   const sections = Array.from(document.querySelectorAll('.hidden'))
-//   const animationDuration = 10000 // 각 섹션의 애니메이션 지속 시간 (밀리초)
-//   let currentIndex = 0 // 현재 활성화된 섹션의 인덱스
-
-//   function scrollToNextSection() {
-//     if (currentIndex >= sections.length) {
-//       return // 모든 섹션을 이미 처리한 경우 종료
-//     }
-
-//     const currentSection = sections[currentIndex]
-//     currentSection.classList.add('active') // 섹션을 활성화 (보이도록)합니다.
-
-//     // 이전 섹션을 비활성화 (숨김)합니다.
-//     if (currentIndex > 0) {
-//       sections[currentIndex - 1].classList.remove('active')
-//     }
-
-//     // 다음 섹션으로 스크롤 애니메이션을 적용합니다.
-//     const sectionOffsetTop = currentSection.offsetTop
-//     const scrollDistance = sectionOffsetTop - window.scrollY
-//     const framesPerSecond = 60
-//     const totalFrames = (animationDuration / 1000) * framesPerSecond
-//     const scrollStep = scrollDistance / totalFrames
-
-//     let currentFrame = 0
-//     const scrollInterval = setInterval(function () {
-//       if (currentFrame >= totalFrames) {
-//         // 애니메이션 종료 후 다음 섹션으로 이동
-//         currentIndex++
-//         scrollToNextSection()
-//         clearInterval(scrollInterval)
-//       } else {
-//         window.scrollBy(0, scrollStep)
-//         currentFrame++
-//       }
-//     }, 1000 / framesPerSecond)
-//   }
-
-//   // 첫 번째 섹션을 활성화하고 애니메이션을 시작합니다.
-//   sections[0].classList.add('active')
-//   scrollToNextSection()
-// })
+const lineLastImage = document.querySelector('.line_four :first-child')
 
 window.document.addEventListener('DOMContentLoaded', () => {
   scroll.style.animationPlayState = 'paused'
@@ -71,6 +26,7 @@ const observerEnd = new IntersectionObserver((entries) => {
       setTimeout(() => {
         scroll.style.animationPlayState = 'paused'
         animationPaused = true
+        lineLastImage.src = './images/svg/top.svg'
       }, 1500)
     }
   })
@@ -78,3 +34,18 @@ const observerEnd = new IntersectionObserver((entries) => {
 
 observer.observe(targetElement)
 observerEnd.observe(end)
+
+lineLastImage.addEventListener('click', () => {
+  const imageSrcArr = lineLastImage.src.split('/')
+  const imageSrc = imageSrcArr[imageSrcArr.length - 1]
+
+  if (imageSrc === 'down.svg') return
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+
+  lineLastImage.src = './images/svg/down.svg'
+})
