7f68474bf66deeaaf802f3295af239e17393cd42
pinomaker-hoo <inhoo23@naver.com>
2023-10-31 20:05:37 +0900

    feat : End Page

--- src/js/end.js
+++ src/js/end.js
@@ -1,3 +1,10 @@
+const content = document.querySelector('.content')
+const targetElement = document.querySelector('.start_point')
+const scroll = document.querySelector('.scrolling-text')
+const end = document.querySelector('.stop_point')
+
+let scrollPosition = 0
+
 // window.addEventListener('DOMContentLoaded', function () {
 //   const sections = Array.from(document.querySelectorAll('.hidden'))
 //   const animationDuration = 10000 // 각 섹션의 애니메이션 지속 시간 (밀리초)
@@ -42,30 +49,32 @@
 //   scrollToNextSection()
 // })
 
-const content = document.querySelector('.content')
-let scrollPosition = 0
-
-// 스크롤 이벤트 리스너 추가
-document.querySelector('.scroll-box').addEventListener('scroll', function () {
-  const scrollTop = this.scrollTop
-  const scrollHeight = this.scrollHeight - this.clientHeight
-
-  // 스크롤 위치를 퍼센트로 계산
-  const scrollPercentage = (scrollTop / scrollHeight) * 100
-
-  // 서서히 스크롤 이벤트 처리
-  content.style.transform = `translateY(-${scrollPercentage}%)`
+window.document.addEventListener('DOMContentLoaded', () => {
+  scroll.style.animationPlayState = 'paused'
+  animationPaused = true
 })
-
-const targetElement = document.querySelector('#four_content')
-const scroll = document.querySelector('.scrolling-text')
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      scroll.style.animation = 'scrollText 100s linear infinite;' // 애니메이션 시작
+      setTimeout(() => {
+        scroll.style.animationPlayState = 'running'
+        animationPaused = false
+      }, 1000)
+    }
+  })
+})
+
+const observerEnd = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        scroll.style.animationPlayState = 'paused'
+        animationPaused = true
+      }, 1500)
     }
   })
 })
 
 observer.observe(targetElement)
+observerEnd.observe(end)
