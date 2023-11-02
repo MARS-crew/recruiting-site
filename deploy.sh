60a9e262ddca0df5585cd88cb71a8e60173bec84
pinomaker-hoo <inhoo23@naver.com>
2023-11-02 18:06:54 +0900

    ㄹ

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+npm run build
+
+cp -r ./dist/index.html ./index.html
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
-export default {}
+// vite.config.js
+
+export default {
+  base: 'src', // 루트 경로를 설정합니다.
+  build: {
+    // 빌드 설정...
+  },
+}
